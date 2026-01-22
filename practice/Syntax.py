252cdb94fc171df02f551f80eb6f1a33160a1b1e
sa1ovaa <a_sabyrova@kbtu.kz>
2026-01-22 21:54:42 +0600

    Add Practice1 Python basics examples

--- practice/Syntax.py
+++ practice/Syntax.py
@@ -1,2 +1,10 @@
+#1 True or False: Indentation in Python is for readability only.:False
+#2
 if 5 > 2:
   print("Five is greater than two!")
+#3 Syntax Error:
+if 5 > 2:
+print("Five is greater than two!")
+#4 In Python, variables are created when you assign a value to it:
+x = 5
+y = "Hello, World!"
