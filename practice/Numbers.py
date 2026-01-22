b69f2891cb45f737f728c0ffa586046366edfeb7
sa1ovaa <a_sabyrova@kbtu.kz>
2026-01-22 21:29:20 +0600

    Add files via upload

---
+++ practice/Numbers.py
@@ -0,0 +1,17 @@
+#There are three numeric types in Python:
+# int
+# float
+# complex
+
+#1
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#2
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ practice/Variables.py
@@ -0,0 +1,29 @@
+#1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#3
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#4 You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#5 Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
